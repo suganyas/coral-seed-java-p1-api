4064741acb9d25b779a80e3acd9f1010c0af9669
Erik Schlegel <erik.schlegel@gmail.com>
2022-12-12 18:52:59 +0000

    fixing dev container issue

--- .devcontainer/scripts/codespace-postCreate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-sudo chown codespace -R ./.git/hooks
-
-. "$SCRIPT_DIR/detect-secrets/init.sh"
-. "$SCRIPT_DIR/cluster-create.sh"
-. "$SCRIPT_DIR/cluster-runApp.sh"
--- .devcontainer/scripts/codespace-postStart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir ./.devcontainer/logs/ -p
-nohup bash -c 'octant --disable-origin-check --disable-open-browser &' > ./.devcontainer/logs/octant.log
